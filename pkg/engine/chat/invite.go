9577f5de4dad2544c2bf581b31e4dcd3d8919876
natyosu3 <nachi33482@gmail.com>
2024-09-28 17:30:39 +0000

    Refactor SocketFormat struct and add FirstConnectionType

--- pkg/engine/chat/invite.go
+++ pkg/engine/chat/invite.go
@@ -2,12 +2,10 @@ package chat
 
 import (
 	"log"
-	"log/slog"
 	"net/http"
 	"sync"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
@@ -42,11 +40,11 @@ type User struct {
 }
 
 type SocketFormat struct {
-	Type        SocketType `json:"type"`
-	ConectionID string     `json:"conection_id"`
-	Data        string     `json:"data"`
-	Sender      User       `json:"sender"`
-	Receiver    User       `json:"receiver"`
+	Type         SocketType `json:"type"`
+	ConnectionID string     `json:"connection_id"`
+	Data         string     `json:"data"`
+	Sender       User       `json:"sender"`
+	Receiver     User       `json:"receiver"`
 }
 
 var connections = struct {
@@ -62,20 +60,47 @@ func createChatRoom(c *gin.Context) {
 	}
 	defer conn.Close()
 
-	// 一意のIDを生成
-	connID := uuid.New().String()
+	// 最初のメッセージでユーザIDを受信
+	var initialMsg SocketFormat
+	err = conn.ReadJSON(&initialMsg)
+	if err != nil {
+		log.Println("Failed to read initial message: ", err)
+		return
+	}
+
+	// ユーザIDをコネクションIDとして使用
+	userID := initialMsg.Sender.ID
 
 	// 接続を管理するマップに追加
 	connections.Lock()
-	connections.m[connID] = conn
+	connections.m[userID] = conn
 	connections.Unlock()
 
 	defer func() {
 		// 接続を管理するマップから削除
 		connections.Lock()
-		delete(connections.m, connID)
+		delete(connections.m, userID)
 		connections.Unlock()
 	}()
+
+	// 最初のメッセージを処理
+	if initialMsg.Type == FirstConnectionType {
+		sendMessage(conn, SocketFormat{
+			Type:         MessageType,
+			Data:         "Message received by server",
+			ConnectionID: userID,
+			Sender: User{
+				ID:   initialMsg.Sender.ID,
+				Name: initialMsg.Sender.Name,
+				UUID: userID,
+			},
+			Receiver: User{
+				ID:   "admin",
+				Name: "admin",
+				UUID: "admin",
+			},
+		})
+	}
 
 	for {
 		var msg SocketFormat
@@ -86,61 +111,51 @@ func createChatRoom(c *gin.Context) {
 		}
 
 		if !allowedTypes[msg.Type] {
-			log.Println("Invalid message type: ", msg.Type)
-			err = conn.WriteJSON(SocketFormat{
-				Type: ErrorType,
-				Data: "Invalid message type",
-			})
-			if err != nil {
-				log.Println("Write error: ", err)
-				break
-			}
+			sendError(conn, "Invalid message type")
 			continue
 		}
 
 		switch msg.Type {
-		case FirstConnectionType:
-			// メッセージ送信者にメッセージを送信
-			err = conn.WriteJSON(SocketFormat{
-				Type:        MessageType,
-				Data:        "Message received by server",
-				ConectionID: connID,
-				Sender: User{
-					ID:   msg.Sender.ID,
-					Name: msg.Sender.Name,
-					UUID: connID,
-				},
-				Receiver: User{
-					ID:   "admin",
-					Name: "admin",
-					UUID: "admin",
-				},
-			})
-			if err != nil {
-				log.Println("Write error: ", err)
-			}
 		case MessageType:
-			log.Printf("Message from %s: %s", msg.Sender, msg.Data)
+			log.Printf("Message from %s: %s", msg.Sender.Name, msg.Data)
 		case InviteType:
-			log.Printf("Invite from %s to %s: %s", msg.Sender, msg.Receiver, msg.Data)
-			// 受信者に通知を送信
-			connections.RLock()
-			receiverConn, ok := connections.m[msg.Receiver.ID]
-			connections.RUnlock()
-			if ok {
-				err = receiverConn.WriteJSON(SocketFormat{
-					Type:   InviteType,
-					Data:   msg.Data,
-					Sender: msg.Sender,
-				})
-				if err != nil {
-					log.Println("Write error: ", err)
-				}
-			} else {
-				slog.Info("Receiver not found: " + msg.Receiver.ID)
-			}
+			log.Printf("Invite from %s to %s: %s", msg.Sender.Name, msg.Receiver.Name, msg.Data)
+			notifyReceiver(msg)
 		case ErrorType:
-			log.Printf("Error from %s: %s", msg.Sender, msg.Data)
+			log.Printf("Error from %s: %s", msg.Sender.Name, msg.Data)
 		}
+	}
+}
+
+func sendError(conn *websocket.Conn, errorMsg string) {
+	err := conn.WriteJSON(SocketFormat{
+		Type: ErrorType,
+		Data: errorMsg,
+	})
+	if err != nil {
+		log.Println("Write error: ", err)
+	}
+}
+
+func sendMessage(conn *websocket.Conn, msg SocketFormat) {
+	err := conn.WriteJSON(msg)
+	if err != nil {
+		log.Println("Write error: ", err)
+	}
+}
+
+func notifyReceiver(msg SocketFormat) {
+	connections.RLock()
+	receiverConn, ok := connections.m[msg.Receiver.ID]
+	connections.RUnlock()
+	if ok {
+		sendMessage(receiverConn, SocketFormat{
+			Type:     InviteType,
+			Data:     msg.Data,
+			Sender:   msg.Sender,
+			Receiver: msg.Receiver,
+		})
+	} else {
+		log.Printf("Receiver not found: %s", msg.Receiver.ID)
 	}
 }
